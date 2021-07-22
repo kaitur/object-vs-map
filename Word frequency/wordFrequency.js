84d4be240a84088880d8767f8574d78945fbff9d
kaitur <artur.kaidanskyi@gmail.com>
2021-07-22 03:25:38 +0300

    using Map and UnitTest in repetition search function

--- Word frequency/wordFrequency.js
+++ Word frequency/wordFrequency.js
@@ -1,24 +1,29 @@
-let text = "JavaScript (JS)  13 15 can`t is a lightweight, interpreted, or just-in-time compiled programming language with first-class functions. While it is most well-known as the scripting language for Web pages, many non-browser environments also use it, such as Node.js, Apache CouchDB and Adobe Acrobat. JavaScript is a prototype-based, multi-paradigm, single-threaded, dynamic language, supporting object-oriented, imperative, and declarative (e.g. functional programming) styles. Read more about JavaScript.";
+let text = "JavaScript (JS) js 13 15 can`t is a lightweight, interpreted, or just-in-time compiled programming language with first-class functions. While it is most well-known as the 13 15 scripting language for Web pages, many non-browser environments also use it, such as Node.js, Apache CouchDB and Adobe Acrobat. JavaScript is a prototype-based, multi-paradigm, single-threaded, dynamic language, supporting object-oriented, imperative, 13 and declarative (functional programming) styles. 13 Read more about JavaScript.";
 
 function SearchForDuplicates(text) {
-    let words = textEditing(text).split(" ");
+    let arrayOfWords = creatingAnArrayOfWords(text)
     const wordMap = new Map();
 
-    for (let i = 0; i < words.length; i++) {
-        let currentWordCount = wordMap.set("words", words[i]);
+    for (let i = 0; i < arrayOfWords.length; i++) {
+        let currentWordCount = wordMap.get(arrayOfWords[i]);
         let count = currentWordCount ? currentWordCount : 0;
-       //wordMap.set(words[i], count + 1)
-        wordMap.set("words", `${words[i]} ${ count + 1}`)
-        console.log(wordMap.get("words"));
+        wordMap.set(arrayOfWords[i], count + 1)
     }
     return wordMap;
 }
 
-function textEditing(text) {
-    let str = text;
-    return str = str.replace(/[^a-zа-яё0-9`\s]/gi, '');
+function stringReplacement(text) {
+    let strReplacement = text.replace(/[^a-zа-яё0-9`-\s]/gi, '').toLowerCase();
+    return strReplacement;
 }
 
-console.log(SearchForDuplicates(text));
+function creatingAnArrayOfWords(text) {
+    let arrayOfWords = stringReplacement(text).split(" ");
+    return arrayOfWords;
+}
 
-export default SearchForDuplicates;
+function consoleOutput() {console.log(SearchForDuplicates(text));}
+
+consoleOutput();
+
+export {creatingAnArrayOfWords as funcOfCreating, stringReplacement as funcOfReplacement};
--- Word frequency/wordFrequency.test.js
+++ Word frequency/wordFrequency.test.js
@@ -1,10 +1,15 @@
-import SearchForDuplicates from './wordFrequency';
+import * as ws from './wordFrequency.js';
 describe('word frequency', () => {
-    const text = "JavaScript (JS) can`t is a lightweight, interpreted, or just-in-time compiled programming language with first-class functions. While it is most well-known as the scripting language for Web pages, many non-browser environments also use it, such as Node.js, Apache CouchDB and Adobe Acrobat. JavaScript is a prototype-based, multi-paradigm, single-threaded, dynamic language, supporting object-oriented, imperative, and declarative (e.g. functional programming) styles. Read more about JavaScript.";
-    test('reading array', () => {
-        expect(SearchForDuplicates(text)).not.toHaveProperty('');
-    })
-    test('search symbol `', () => {
-        expect(SearchForDuplicates(text)).toHaveProperty('can`t');
-    })
-})
+    const text = "JavaScript (JS) js 13 15 can`t is a lightweight, interpreted, or just-in-time compiled programming language with first-class functions. While it is most well-known as the 13 15 scripting language for Web pages, many non-browser environments also use it, such as Node.js, Apache CouchDB and Adobe Acrobat. JavaScript is a prototype-based, multi-paradigm, single-threaded, dynamic language, supporting object-oriented, imperative, 13 and declarative (functional programming) styles. 13 Read more about JavaScript.";
+    test('matches if the actual array does not contain the expected elements', () => {
+        expect(ws.funcOfCreating(text)).toEqual(expect.not.arrayContaining(['']),
+        );
+    });
+    test('matches if the received value does not contain the expected substring', () => {
+        expect(ws.funcOfReplacement(text)).toEqual(expect.not.stringContaining('.' && ',' && '(' && ')' && '!' && '@' && '#' && '$' && '%' && '^' && '*' && '<' && '>'));
+    });
+    test('matches if the received value contain the expected substring', () => {
+        expect(ws.funcOfReplacement(text)).toEqual(expect.stringContaining('`' && '-' ));
+    });
+});
+//  запитати про різницю між it and test
