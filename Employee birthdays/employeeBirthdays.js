e5184cbdb6fd349b05b710d49b444d3509bd1010
Artur Kaidanskyi <artur.kaidanskyi@gmail.com>
2021-07-23 18:58:42 +0300

    final ver.

--- Employee birthdays/employeeBirthdays.js
+++ Employee birthdays/employeeBirthdays.js
@@ -1,75 +1,99 @@
 const moment = require("moment");
+const csv = require('csv-parser');
+const fs = require('fs');
 
-let monthInAdvance = 11;
 let currentYear = moment().year();
 let currentMonth = moment().month();
-let currentDay = moment().date();
+let index = process.argv[2];
+let filePath = process.argv[3];
 
-function addingEmployeesToTheArray() {
-    let vera = { name: "Бондарева Вера", birthdays: moment([2000, 6, 20]) };
-    let vika = { name: "Васильева Виктория", birthdays: moment([1989, 5, 12]) };
-    let malika = { name: "Кузнецова Малика", birthdays: moment([1981, 4, 12]) };
-    let anna = { name: "Петрова Анна", birthdays: moment([1973, 1, 5]) };
-    let maks = { name: "Белов Максим", birthdays: moment([1984, 1, 7]) };
-    let artem = { name: "Федосеев Артём", birthdays: moment([1971, 1, 28]) };
-    let lila = { name: "Овчинникова Лиля", birthdays: moment([1973, 1, 17]) };
-    let mark = { name: "Емельянов Марк", birthdays: moment([1995, 1, 8]) };
-    let dima = { name: "Попов Дмитрий", birthdays: moment([1986, 1, 4]) };
-    let alisa = { name: "Калмыкова Алиса", birthdays: moment([1998, 1, 25]) };
-    let veronika = { name: "Лобанова Вероника", birthdays: moment([1977, 1, 6]) };
+//#region Reading CSV-file
+let arrayOfEmployees = [];
 
-    let arrayOfEmployees = [vera, vika, malika, anna, maks, artem, lila, mark, dima, alisa, veronika];
-    displayOfTheMonthInText(arrayOfEmployees, 0);
-    numberOfYearse(arrayOfEmployees, 0);
-    return arrayOfEmployees;
-}
+fs.createReadStream(filePath)
+    .pipe(csv())
+    .on('data', (data) => arrayOfEmployees.push(data))
+    .on('end', () => {
+
+        for (let i = 0; i < arrayOfEmployees.length; i++) {
+            arrayOfEmployees[i].birthdays = moment([arrayOfEmployees[i].year, arrayOfEmployees[i].month, arrayOfEmployees[i].day]);
+        }
+        consoleOutput(index, arrayOfEmployees);
+    });
+//#endregion
 
 function numberOfYearse(arrayOfEmployees, n) {
     let numOfYearse = currentYear - arrayOfEmployees[n].birthdays.year();
     return moment().month() <= arrayOfEmployees[n].birthdays.month() ? numOfYearse : ++numOfYearse;
 }
-
+/*
 function displayOfTheMonthInText(arrayOfEmployees, n) {
     let monthInText = arrayOfEmployees[n].birthdays.locale('ru').format('MMMM');
     return monthInText[0].toUpperCase() + monthInText.slice(1);
 }
+*/
 function displayOfTheMonthInText(i) {
     let month = moment().add(i, 'month').locale('ru').format('MMMM');
     let monthInText = month[0].toUpperCase() + month.slice(1);
     return monthInText;
 }
+
 function displayOfTheYear(i) {
     return currentMonth + i <= 11 ? currentYear : currentYear + 1;
 }
 
-function sortByMonth(ii) {
-    if(ii == 11)
-    let arrayOfEmployees = addingEmployeesToTheArray();
-    const sortedArray = arrayOfEmployees.sort((a, b) => { return moment(a.birthdays.month()).diff(b.birthdays.month()) });
+function oversupplyCheck(monthValue) {
+    let monthForWithdrawal = currentMonth + monthValue;
+    return monthForWithdrawal <= 11 ? monthForWithdrawal : monthForWithdrawal - 11;
+}
+
+function arrayEmplCurrMonth(index, arrayOfEmployees) {
+    let sortedArray = sortByMonth(arrayOfEmployees);
     let oneMonthArray = [];
     let count = 0;
+
     for (let i = 0; i < sortedArray.length; i++) {
-        if (sortedArray[i].birthdays.month() == currentMonth + ii) {
+        if (sortedArray[i].birthdays.month() == oversupplyCheck(index)) {
             oneMonthArray[count] = sortedArray[i];
             count++;
         }
     }
+
     return oneMonthArray;
 }
-function consoleOutput() {
-    for (let i = 0; i <= monthInAdvance; i++) {
-        let oneMonthArray = sortByMonth(i);
+
+function sortByMonth(arrayOfEmployees) {
+    let arrayOfEmployeess = arrayOfEmployees;
+    let sortedArray = arrayOfEmployeess.sort((a, b) => { return moment(a.birthdays.month()).diff(b.birthdays.month()) });
+    return sortedArray;
+}
+
+function sortByDay(index, arrayOfEmployees) {
+    let arrayOfEmployeess = arrayEmplCurrMonth(index, arrayOfEmployees);
+    let sortedArrayByDay = arrayOfEmployeess.sort((a, b) => { return moment(b.birthdays.date()).diff(a.birthdays.date()) });
+    return sortedArrayByDay;
+}
+
+function endingOfTheWord(quantity) {
+    if (quantity == 0 || (quantity % 10) == 0 || (quantity >= 5 && quantity <= 20) || (quantity % 10 >= 5 && quantity % 10 <= 20)) return "лет";
+    else if (quantity == 1 || (quantity % 10) == 1) return "год";
+    else if ((quantity >= 2 && quantity <= 4) || (quantity % 10 >= 2 && quantity % 10 <= 4)) return "года";
+}
+
+function consoleOutput(index, arrayOfEmployees) {
+    for (let i = 0; i <= index; i++) {
+        let oneMonthArray = sortByDay(i, arrayOfEmployees);
         let text = `${displayOfTheMonthInText(i)} ${displayOfTheYear(i)}`;
         console.log(text);
-        for (let index = oneMonthArray.length -1; index >= 0 ; index--) {
-            let text_2 = `(${oneMonthArray[index].birthdays.date()}) - ${oneMonthArray[index].name} (${numberOfYearse(oneMonthArray, index)})`; //(${numberOfYearse(sortByMonth(), 0)})`;
+        for (let j = oneMonthArray.length - 1; j >= 0; j--) {
+            let text_2 = `(${oneMonthArray[j].birthdays.date()}) - ${oneMonthArray[j].name} (${numberOfYearse(oneMonthArray, j)} ${endingOfTheWord(numberOfYearse(oneMonthArray, j))})`;
             console.log(text_2);
         }
-            
-        
     }
 }
-consoleOutput();
-addingEmployeesToTheArray();
-
-//export { numberOfYearse as funcNumberOfYearse, displayOfTheMonthInText as funcDisplayOfTheMonthInText};
+/*export {
+    numberOfYearse as funcNumberOfYearse,
+    displayOfTheMonthInText as funcDisplayOfTheMonthInText,
+    displayOfTheYear as funcDisplayOfTheYear,
+    endingOfTheWord as funcEndingOfTheWord
+};*/
--- Employee birthdays/employeeBirthdays.test.js
+++ Employee birthdays/employeeBirthdays.test.js
@@ -1,43 +1,51 @@
 const moment = require("moment");
 import * as eb from './employeeBirthdays.js';
 
-describe('employee birthdays', () => {
-    //#region A list of employees
-    let vera = { name: "Бондарева Вера", birthdays: moment([2000, 7, 20]) };
-    let vika = { name: "Васильева Виктория", birthdays: moment([2000, 6, 20]) };
-    let malika = { name: "Кузнецова Малика", birthdays: moment([2000, 5, 20]) };
-    let anna = { name: "Петрова Анна", birthdays: moment([1973, 1, 5]) };
-    let maks = { name: "Белов Максим", birthdays: moment([1984, 2, 7]) };
-    let artem = { name: "Федосеев Артём", birthdays: moment([1971, 6, 28]) };
-    let lila = { name: "Овчинникова Лиля", birthdays: moment([1973, 1, 17]) };
-    let mark = { name: "Емельянов Марк", birthdays: moment([1995, 11, 8]) };
-    let dima = { name: "Попов Дмитрий", birthdays: moment([1986, 3, 4]) };
-    let alisa = { name: "Калмыкова Алиса", birthdays: moment([1998, 4, 25]) };
-    let veronika = { name: "Лобанова Вероника ", birthdays: moment([1977, 12, 6]) };
-    
+//#region A list of employees
+let vera = { name: "Бондарева Вера", birthdays: moment([2000, 7, 20]) };
+let vika = { name: "Васильева Виктория", birthdays: moment([2000, 6, 20]) };
+let malika = { name: "Кузнецова Малика", birthdays: moment([2000, 5, 20]) };
+let anna = { name: "Петрова Анна", birthdays: moment([1973, 1, 5]) };
+let maks = { name: "Белов Максим", birthdays: moment([1984, 2, 7]) };
+let artem = { name: "Федосеев Артём", birthdays: moment([1971, 6, 28]) };
+let lila = { name: "Овчинникова Лиля", birthdays: moment([1973, 1, 17]) };
+let mark = { name: "Емельянов Марк", birthdays: moment([1995, 11, 8]) };
+let dima = { name: "Попов Дмитрий", birthdays: moment([1986, 3, 4]) };
+let alisa = { name: "Калмыкова Алиса", birthdays: moment([1998, 4, 25]) };
+let veronika = { name: "Лобанова Вероника ", birthdays: moment([1977, 12, 6]) };
 
-    let arrayOfEmployees = [vera, vika, malika, anna, maks, artem, lila, mark, dima, alisa, veronika];
-    //#endregion
-    test('checking the conformity of the number of years (month after the current) ', () => {
+
+let arrayOfEmployees = [vera, vika, malika, anna, maks, artem, lila, mark, dima, alisa, veronika];
+//#endregion
+describe('', () => {
+    it('checking the conformity of the number of years (month after the current) ', () => {
         expect(eb.funcNumberOfYearse(arrayOfEmployees, 0)).toBe(21);
     })
-    test('checking the conformity of the number of years (month corresponds to the current) ', () => {
+    it('checking the conformity of the number of years (month corresponds to the current) ', () => {
         expect(eb.funcNumberOfYearse(arrayOfEmployees, 1)).toBe(21);
     })
-    test('checking the conformity of the number of years (month before the current) ', () => {
+    it('checking the conformity of the number of years (month before the current) ', () => {
         expect(eb.funcNumberOfYearse(arrayOfEmployees, 2)).toBe(22);
     })
-    /*
-    test('check ... ', () => {
-        expect(eb.funcDisplayOfTheMonthInText(arrayOfEmployees, 1)).toBe("июль");
-    })
-    */
-    test('check for capital letter ', () => {
+})
+describe('', () => {
+    it('check for capital letter ', () => {
         expect(eb.funcDisplayOfTheMonthInText(arrayOfEmployees, 1)).toBe("Июль");
     })
-    test('check on undefined. If the string is empty, it will return undefined', () => {
-        for (let i = 0; i <= 10; i++) {
-            expect(eb.funcDisplayOfTheMonthInText(arrayOfEmployees, i)).not.toBe(undefined);
-        }
+    it('check on undefined. If the string is empty, it will return undefined', () => {
+        expect(eb.funcDisplayOfTheMonthInText(arrayOfEmployees, 0)).not.toBe(undefined);
+    })
+});
+describe('', () => {
+    it('', () => {
+        expect(eb.funcDisplayOfTheYear(5)).toBe(2021);
+    })
+    it('', () => {
+        expect(eb.funcDisplayOfTheYear(6)).toBe(2022);
+    })
+});
+describe('', () => {
+    it('', () => {
+        expect(eb.funcEndingOfTheWord(0)).toBe("лет");
     })
 });
